fedc11fafda637e16aff07d11e8b41ac325e5243
Shinmin Hsu <shinminhsu@gmail.com>
2020-11-21 16:43:10 +0800

    convert to dummies

--- main_hsu.py
+++ main_hsu.py
@@ -15,4 +15,15 @@ detail_group = detail.groupby(['month', 'userid'], as_index=False).sum()[select_
 merge_data = pd.merge(detail_group, info, on='userid')
 merge_data = pd.merge(merge_data, label)
 
-merge_data.head()
+gender_dummies = pd.get_dummies(merge_data.gender, columns=[])
+seller_dummies = pd.get_dummies(merge_data.is_seller)
+month_dummies = pd.get_dummies(merge_data.month)
+label_dummies = pd.get_dummies(merge_data.label)
+
+gender_dummies.columns = ['gender_%i'%i for i in range(gender_dummies.shape[1])]
+seller_dummies.columns = ['seller_%i'%i for i in range(seller_dummies.shape[1])]
+month_dummies.columns = ['month_%i'%i for i in range(month_dummies.shape[1])]
+label_dummies.columns = ['label_%i'%i for i in range(label_dummies.shape[1])]
+
+selected_cols = ['userid', 'order_count', 'total_amount']
+final_data = pd.concat([gender_dummies, seller_dummies, month_dummies, merge_data[selected_cols]], axis=1)
